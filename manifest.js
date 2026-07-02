d78d3d88bca92f7541d0e7bb7f323d58a9513ff4
Alihan Durdu <opisyonelpro@gmail.com>
2026-07-02 18:30:43 +0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,16 @@
+window.mediaManifest = {
+    null: [
+        "images/null/01.jpg",
+        "images/null/02.jpg",
+        "images/null/03.jpg",
+        "video/null/gameplay.mp4"
+    ],
+
+    touchandrun: [
+        "images/touchandrun/01.jpg"
+    ],
+
+    cubeoncube: [
+        "images/cubeoncube/01.jpg"
+    ]
+};
